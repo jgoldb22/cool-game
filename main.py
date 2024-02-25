bf27978c80ba7a648e6ab03c3e4cf67b205bd9c8
verystellar <stopcreepingonme@yahoo.com>
2024-02-25 00:35:04 -0600

    real cool functions

--- main.py
+++ main.py
@@ -1,33 +1,35 @@
-# import the pygame module 
 import pygame 
 
-# cool note
-# Define the background colour 
-# using RGB color coding. 
+pygame.init()
+clock = pygame.time.Clock()
 background_colour = (234, 212, 252) 
-  
-# Define the dimensions of 
-# screen object(width,height) 
 screen = pygame.display.set_mode((300, 300)) 
-  
-# Set the caption of the screen 
-pygame.display.set_caption('Geeksforgeeks') 
-  
-# Fill the background colour to the screen 
+pygame.display.set_caption('sex') 
 screen.fill(background_colour) 
-  
-# Update the display using flip 
+dt = 0
+color = (155,30,220)
 pygame.display.flip() 
-  
-# Variable to keep our game loop running 
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 running = True
   
-# game loop 
 while running: 
     
-# for loop through the event queue   
-    for event in pygame.event.get(): 
-      
-        # Check for QUIT event       
+    for event in pygame.event.get():    
         if event.type == pygame.QUIT: 
             running = False
+    
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_pos.y = max(player_pos.y  - 3, 0)
+    if keys[pygame.K_s]:
+        player_pos.y = min(player_pos.y  + 3, screen.get_height()- 1)
+    if keys[pygame.K_a]:
+        player_pos.x = max(player_pos.x  - 3, 0)
+    if keys[pygame.K_d]:
+        player_pos.x = min(player_pos.x  + 3, screen.get_width() -1)
+    if keys[pygame.K_y]:
+        screen.fill(background_colour) 
+        
+    pygame.draw.rect(screen, color, pygame.Rect(player_pos.x, player_pos.y, 4, 4))
+    pygame.display.flip()
+    dt = clock.tick(60) / 1000

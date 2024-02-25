76dc813d2834500d28df1dd0d91b34cf5de818a1
verystellar <stopcreepingonme@yahoo.com>
2024-02-25 01:16:57 -0600

    Reapply "real cool functions"
    This reverts commit 8b926c38ead5167b50b53748c40a551bf27b2d82.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame 
+import random
 
 pygame.init()
 clock = pygame.time.Clock()
@@ -8,16 +9,19 @@ pygame.display.set_caption('sex')
 screen.fill(background_colour) 
 dt = 0
 color = (155,30,220)
+color1 = (255,0,0)
+count = 0
 pygame.display.flip() 
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 running = True
-  
+
+circle_pos = pygame.Vector2(screen.get_width() / 2, 0)
 while running: 
     
     for event in pygame.event.get():    
         if event.type == pygame.QUIT: 
             running = False
-    
+    screen.fill(background_colour) 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
         player_pos.y = max(player_pos.y  - 3, 0)
@@ -27,9 +31,12 @@ while running:
         player_pos.x = max(player_pos.x  - 3, 0)
     if keys[pygame.K_d]:
         player_pos.x = min(player_pos.x  + 3, screen.get_width() -1)
-    if keys[pygame.K_y]:
-        screen.fill(background_colour) 
-        
-    pygame.draw.rect(screen, color, pygame.Rect(player_pos.x, player_pos.y, 4, 4))
+    if (circle_pos.y == 0):
+        circle_pos.x = random.randint(0, screen.get_width())
+    circle_pos.y = (circle_pos.y + 3) % screen.get_height()
+    pygame.draw.circle(screen, color1, circle_pos, 3)
+    pygame.draw.rect(screen, color, pygame.Rect(player_pos.x, player_pos.y, 20, 4))
     pygame.display.flip()
-    dt = clock.tick(60) / 1000
+    count = pygame.time.get_ticks() / 100
+    dt = clock.tick(60) / 10000
+    print(count)
